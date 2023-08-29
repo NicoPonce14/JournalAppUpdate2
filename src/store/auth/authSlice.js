e0cee5d854c363c099ac1d3369c0069aa90691c8
Nicolas Ponce <nponcech@gmail.com>
2023-08-28 23:55:17 -0400

    commit redux install

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    status:'checking',
+    uid:null,
+    email:null,
+    displayName:null,
+    photoUrl:null,
+    errorMessage:null
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login:(state,action) => {
+
+    },
+    logout:(state,payload)=>{
+
+    },
+    checkingCredentials: (state) =>{
+
+    }
+
+  }
+});
+
+export const {login,logout,checkingCredentials} = authSlice.actions
+
